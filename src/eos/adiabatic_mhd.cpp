bacf804b4f89fa64ce063749efb41d64594c4be7
Drummond Fielding <drummondfielding@gmail.com>
2018-12-11 12:54:06 -0500

    added neighbor flooring and velocity ceilings to adiabatic mhd eos

--- src/eos/adiabatic_mhd.cpp
+++ src/eos/adiabatic_mhd.cpp
@@ -27,6 +27,8 @@ EquationOfState::EquationOfState(MeshBlock *pmb, ParameterInput *pin) {
   gamma_ = pin->GetReal("hydro", "gamma");
   density_floor_  = pin->GetOrAddReal("hydro", "dfloor", std::sqrt(1024*(FLT_MIN)));
   pressure_floor_ = pin->GetOrAddReal("hydro", "pfloor", std::sqrt(1024*(FLT_MIN)));
+  velocity_ceiling_ = pin->GetOrAddReal("hydro","vceil",0.0);
+  neighbor_flooring_ = pin->GetOrAddBoolean("hydro","neighbor_flooring_", false); // average the neighboring cells to get floor value rather than reset to the floor value
 }
 
 // destructor
@@ -67,9 +69,41 @@ void EquationOfState::ConservedToPrimitive(AthenaArray<Real> &cons,
       Real& w_p  = prim(IPR,k,j,i);
 
       // apply density floor, without changing momentum or energy
-      u_d = (u_d > density_floor_) ?  u_d : density_floor_;
-      w_d = u_d;
+      if (neighbor_flooring_ == false) {
+        u_d = (u_d > density_floor_) ?  u_d : density_floor_;
+      } else {
+        if (u_d < density_floor_){
+          Real n_neighbors = 0.0; 
+          Real d_neighbors = 0.0; 
+          if ((k+1 <= ku+NGHOST)&&(cons(IDN,k+1,j,i)>density_floor_)){
+            n_neighbors += 1.0;
+            d_neighbors += cons(IDN,k+1,j,i);
+          }
+          if ((j+1 <= ju+NGHOST)&&(cons(IDN,k,j+1,i)>density_floor_)){
+            n_neighbors += 1.0;
+            d_neighbors += cons(IDN,k,j+1,i);
+          }
+          if ((i+1 <= iu+NGHOST)&&(cons(IDN,k,j,i+1)>density_floor_)){
+            n_neighbors += 1.0;
+            d_neighbors += cons(IDN,k,j,i+1);
+          }
+          if ((k-1 >= kl-NGHOST)&&(cons(IDN,k-1,j,i)>density_floor_)){
+            n_neighbors += 1.0;
+            d_neighbors += cons(IDN,k-1,j,i);
+          }
+          if ((j-1 >= jl-NGHOST)&&(cons(IDN,k,j-1,i)>density_floor_)){
+            n_neighbors += 1.0;
+            d_neighbors += cons(IDN,k,j-1,i);
+          }
+          if ((i-1 >= il-NGHOST)&&(cons(IDN,k,j,i-1)>density_floor_)){
+            n_neighbors += 1.0;
+            d_neighbors += cons(IDN,k,j,i-1);
+          }
+          u_d = (d_neighbors/n_neighbors > density_floor_) ? d_neighbors/n_neighbors : density_floor_;
+        }
+      }
 
+      w_d = u_d;
       Real di = 1.0/u_d;
       w_vx = u_m1*di;
       w_vy = u_m2*di;
@@ -84,8 +118,124 @@ void EquationOfState::ConservedToPrimitive(AthenaArray<Real> &cons,
       w_p = gm1*(u_e - ke - pb);
 
       // apply pressure floor, correct total energy
-      u_e = (w_p > pressure_floor_) ?  u_e : ((pressure_floor_/gm1) + ke + pb);
-      w_p = (w_p > pressure_floor_) ?  w_p : pressure_floor_;
+      if (neighbor_flooring_ == false) {
+        u_e = (w_p > pressure_floor_) ?  u_e : ((pressure_floor_/gm1) + ke + pb);
+        w_p = (w_p > pressure_floor_) ?  w_p : pressure_floor_;
+      } else {
+        if (w_p < pressure_floor_) {
+          Real n_neighbors = 0.0; 
+          Real p_neighbors = 0.0; 
+          if ((k+1 <= ku+NGHOST)&&(prim(IPR,k+1,j,i)>pressure_floor_)){
+            n_neighbors += 1.0;
+            p_neighbors += prim(IPR,k+1,j,i);
+          }
+          if ((j+1 <= ju+NGHOST)&&(prim(IPR,k,j+1,i)>pressure_floor_)){
+            n_neighbors += 1.0;
+            p_neighbors += prim(IPR,k,j+1,i);
+          }
+          if ((i+1 <= iu+NGHOST)&&(prim(IPR,k,j,i+1)>pressure_floor_)){
+            n_neighbors += 1.0;
+            p_neighbors += prim(IPR,k,j,i+1);
+          }
+          if ((k-1 >= kl-NGHOST)&&(prim(IPR,k-1,j,i)>pressure_floor_)){
+            n_neighbors += 1.0;
+            p_neighbors += prim(IPR,k-1,j,i);
+          }
+          if ((j-1 >= jl-NGHOST)&&(prim(IPR,k,j-1,i)>pressure_floor_)){
+            n_neighbors += 1.0;
+            p_neighbors += prim(IPR,k,j-1,i);
+          }
+          if ((i-1 >= il-NGHOST)&&(prim(IPR,k,j,i-1)>pressure_floor_)){
+            n_neighbors += 1.0;
+            p_neighbors += prim(IPR,k,j,i-1);
+          }
+          u_e = (p_neighbors/n_neighbors > pressure_floor_) ?  u_e : ((pressure_floor_/gm1) + ke + pb);
+          w_p = (p_neighbors/n_neighbors > pressure_floor_) ?  p_neighbors/n_neighbors : pressure_floor_;
+        }
+      }
+      // apply velocity magnitude ceiling, correct total energy
+      if (velocity_ceiling_ > 0.0) {
+        if (neighbor_flooring_ == false) {
+          if (std::abs(w_vx) > velocity_ceiling_){
+            u_e -= 0.5 * di * SQR(u_m1);
+            w_vx = w_vx/std::abs(w_vx) * velocity_ceiling_ ;
+            u_m1 = w_vx/std::abs(w_vx) * velocity_ceiling_ * u_d;
+            u_e += 0.5 * di * SQR(u_m1);
+          }
+          if (std::abs(w_vy) > velocity_ceiling_){
+            u_e -= 0.5 * di * SQR(u_m2);
+            w_vy = w_vy/std::abs(w_vy) * velocity_ceiling_ ;
+            u_m2 = w_vy/std::abs(w_vy) * velocity_ceiling_ * u_d;
+            u_e += 0.5 * di * SQR(u_m2);
+          }
+          if (std::abs(w_vz) > velocity_ceiling_){
+            u_e -= 0.5 * di * SQR(u_m3);
+            w_vz = w_vz/std::abs(w_vz) * velocity_ceiling_ ;
+            u_m3 = w_vz/std::abs(w_vz) * velocity_ceiling_ * u_d;
+            u_e += 0.5 * di * SQR(u_m3);
+          }
+        } else {
+          if ((std::abs(w_vx) > velocity_ceiling_)||(std::abs(w_vy) > velocity_ceiling_)||(std::abs(w_vz) > velocity_ceiling_)){
+            std::cout << " v > velocity_ceiling_ " << " u_d = " << u_d << " u_e = " << u_e <<  " w_vx = " << w_vx << " w_vy = " << w_vy <<  " w_vz = " << w_vz <<   " i j k = " << i << " " << j << " " << k << " " << " x y z = " << pco->x1v(i) << " " << pco->x2v(j) << " " << pco->x3v(k) << " " << "\n";
+            Real n_neighbors = 0.0;
+            Real m1_neighbors = 0.0;
+            Real m2_neighbors = 0.0;
+            Real m3_neighbors = 0.0;
+            if ((k+1 <= ku+NGHOST)&&(std::abs(prim(IVX,k+1,j,i))<velocity_ceiling_)&&(std::abs(prim(IVY,k+1,j,i))<velocity_ceiling_)&&(std::abs(prim(IVZ,k+1,j,i))<velocity_ceiling_)){
+              n_neighbors += 1.0;
+              m1_neighbors += cons(IM1,k+1,j,i);
+              m2_neighbors += cons(IM2,k+1,j,i);
+              m3_neighbors += cons(IM3,k+1,j,i);
+            }
+            if ((j+1 <= ju+NGHOST)&&(std::abs(prim(IVX,k,j+1,i))<velocity_ceiling_)&&(std::abs(prim(IVY,k,j+1,i))<velocity_ceiling_)&&(std::abs(prim(IVZ,k,j+1,i))<velocity_ceiling_)){
+              n_neighbors += 1.0;
+              m1_neighbors += cons(IM1,k,j+1,i);
+              m2_neighbors += cons(IM2,k,j+1,i);
+              m3_neighbors += cons(IM3,k,j+1,i);
+            }
+            if ((i+1 <= iu+NGHOST)&&(std::abs(prim(IVX,k,j,i+1))<velocity_ceiling_)&&(std::abs(prim(IVY,k,j,i+1))<velocity_ceiling_)&&(std::abs(prim(IVZ,k,j,i+1))<velocity_ceiling_)){
+              n_neighbors += 1.0;
+              m1_neighbors += cons(IM1,k,j,i+1);
+              m2_neighbors += cons(IM2,k,j,i+1);
+              m3_neighbors += cons(IM3,k,j,i+1);
+            }
+            if ((k-1 >= kl-NGHOST)&&(std::abs(prim(IVX,k-1,j,i))<velocity_ceiling_)&&(std::abs(prim(IVY,k-1,j,i))<velocity_ceiling_)&&(std::abs(prim(IVZ,k-1,j,i))<velocity_ceiling_)){
+              n_neighbors += 1.0;
+              m1_neighbors += cons(IM1,k-1,j,i);
+              m2_neighbors += cons(IM2,k-1,j,i);
+              m3_neighbors += cons(IM3,k-1,j,i);
+            }
+            if ((j-1 >= jl-NGHOST)&&(std::abs(prim(IVX,k,j-1,i))<velocity_ceiling_)&&(std::abs(prim(IVY,k,j-1,i))<velocity_ceiling_)&&(std::abs(prim(IVZ,k,j-1,i))<velocity_ceiling_)){
+              n_neighbors += 1.0;
+              m1_neighbors += cons(IM1,k,j-1,i);
+              m2_neighbors += cons(IM2,k,j-1,i);
+              m3_neighbors += cons(IM3,k,j-1,i);
+            }
+            if ((i-1 >= il-NGHOST)&&(std::abs(prim(IVX,k,j,i-1))<velocity_ceiling_)&&(std::abs(prim(IVY,k,j,i-1))<velocity_ceiling_)&&(std::abs(prim(IVZ,k,j,i-1))<velocity_ceiling_)){
+              n_neighbors += 1.0;
+              m1_neighbors += cons(IM1,k,j,i-1);
+              m2_neighbors += cons(IM2,k,j,i-1);
+              m3_neighbors += cons(IM3,k,j,i-1);
+            }
+            u_e -= ke;
+            if (n_neighbors>0.0){
+              u_m1 = m1_neighbors/n_neighbors;
+              u_m2 = m2_neighbors/n_neighbors;
+              u_m3 = m3_neighbors/n_neighbors;
+            } else {
+              u_m1 = 0.0 ;
+              u_m2 = 0.0 ;
+              u_m3 = 0.0 ;
+            }
+            w_vx = u_m1*di;
+            w_vy = u_m2*di;
+            w_vz = u_m3*di;
+            ke = 0.5*di*(SQR(u_m1) + SQR(u_m2) + SQR(u_m3));
+            u_e += ke; 
+            std::cout << " after vceil u_e " << u_e << " w_vx " << w_vx << " w_vy " << w_vy << " w_vz " << w_vz << " n_neighbors " << n_neighbors << "\n";
+          }
+        }
+      }
     }
   }}
 
