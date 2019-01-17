19666fa51c7c560ac51d829a6a2802eeb1c27b0d
Drummond Fielding <drummondfielding@gmail.com>
2019-01-17 15:58:15 -0500

    trying to get initial density perturbations to work

--- src/main.cpp
+++ src/main.cpp
@@ -382,7 +382,7 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    if (pmesh->turb_flag > 1) pmesh->ptrbd->Driving(); // driven turbulence
+    if ((turb_flag == 2) || (turb_flag == 3)) pmesh->ptrbd->Driving(); // driven turbulence
 
     for (int stage=1; stage<=ptlist->nstages; ++stage) {
       if (SELF_GRAVITY_ENABLED == 1) // fft (flag 0 for discrete kernel, 1 for continuous)
