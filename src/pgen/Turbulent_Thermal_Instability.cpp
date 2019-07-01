78208e33697c85d20490ebeb433867d6b20ad37e
Drummond Fielding <drummondfielding@gmail.com>
2019-07-01 10:54:50 -0400

    wait to ramp down cooling until tcoolstart

--- src/pgen/Turbulent_Thermal_Instability.cpp
+++ src/pgen/Turbulent_Thermal_Instability.cpp
@@ -345,7 +345,7 @@ void Cooling_Source_Function(MeshBlock *pmb, const Real t, const Real dt,
 {
   // Readjust cooling if necessary
   if ( Lambda_ramp_down ){
-    if ( fmod(t,dt_Lambda_ramp_down) < dt ) {
+    if (( fmod(t,dt_Lambda_ramp_down) < dt ) && (t > t_cool_start)){
       Lambda_cool -= delta_Lambda_ramp_down;
       if(Globals::my_rank==0) {
         std::cout << "decreased cooling, Lambda_cool = " << Lambda_cool << "\n";
